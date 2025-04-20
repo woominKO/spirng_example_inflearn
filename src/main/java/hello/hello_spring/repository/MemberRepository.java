44b7ff4787e76ede147afb6336790a41d6901d19
Woomin Ko <woomin422@gmail.com>
2025-04-21 07:52:05 +0900

    dd

---
+++ src/main/java/hello/hello_spring/repository/MemberRepository.java
@@ -0,0 +1,14 @@
+package hello.hello_spring.repository;
+
+import hello.hello_spring.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+@Repository
+public interface MemberRepository  {
+    Member save(Member member);
+    Optional<Member> findById(long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
